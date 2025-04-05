6ad72c4a5f84b75450543bf8b79cdcba0b11bc4a
Urbina2082 <urbinaherrerahectorarnoldo@gmail.com>
2025-04-04 22:02:47 -0600

    calculadora terminada

--- app.js
+++ app.js
@@ -7,7 +7,27 @@ buttons.forEach((item) => {
       display.innerText = "";
     } else if (item.id == "back-space") {
       let string = display.innerText.toString();
-      display.innerText = string.substr(0,string.length-1);  
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Null";
+
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
     }
   };
 });
+
+const theme_toggle = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculaitor");
+
+
+let isdark = true;
+
+theme_toggle.onclick = () => {
+  calculator.classList.toggle("dark");
+  theme_toggle, classList.toggle("active");
+  isdark = !isdark;
+};
